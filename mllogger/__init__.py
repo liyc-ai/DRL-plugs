4f82cd62af72befb8045c67599bbb1b02cd226b4
BepfCp <ychenli.x@gmail.com>
2024-01-19 17:35:39 +0800

    add abstract mllogger type

--- mllogger/__init__.py
+++ mllogger/__init__.py
@@ -1,9 +1,12 @@
 __version__ = "1.2.0"
 
 from loguru import logger as console_logger
+from typing import Union
 
 from ._archive import archive_logs
 from ._logger import TBLogger, WBLogger
 from ._sync import sync
 
-__all__ = ["TBLogger", "WBLogger", "console_logger", "sync", "archive_logs"]
+LoggerType = Union[TBLogger, WBLogger]
+
+__all__ = ["LoggerType", "TBLogger", "WBLogger", "console_logger", "sync", "archive_logs"]
